1217912d218ad42bffa859a88a96b2dc58e92150
Obiamaka S Agbaneje <obiamaka.agbaneje@gmail.com>
2017-05-18 15:35:18 +0100

    More refactoring of main.py

--- src/main.py
+++ src/main.py
@@ -27,9 +27,9 @@ def third_home():
     number_of_times = request.args.get('number_of_times')
     if not number_of_times or number_of_times.isdigit() is False:
         number_of_times = 1
-    if username or surname:
-         username = username + ' ' + surname
-    else:
-        username = 'World'
+    if not(username or surname):
+         username = 'World'
+    username = username + ' ' + surname
+
     return render_template('index3.html', username = username, number_of_times = int(number_of_times))
 
