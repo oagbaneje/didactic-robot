4213780a82935285d296537fe9b70b0010e56bff
Obiamaka S Agbaneje <obiamaka.agbaneje@gmail.com>
2017-05-20 11:45:36 +0100

    Created Googl index page

--- src/main.py
+++ src/main.py
@@ -6,29 +6,4 @@ def home():
     username = request.args.get('username')
     return render_template('index.html', username = username)
 
-@app.route('/second_home')
-def second_home():
-    username = request.args.get('username') 
-    surname =  request.args.get('surname')
-    if username and surname:
-         username=username + ' ' + surname
-    elif username and not surname:
-        username=username
-    elif not username and surname:
-       username=surname
-    else:
-        username = 'World'
-    return render_template('index2.html', username=username)
-
-@app.route('/third_home')
-def third_home():
-    username = request.args.get('username', default='') 
-    surname =  request.args.get('surname', default='')
-    number_of_times = request.args.get('number_of_times', default='1')
-    if number_of_times.isdigit() is False:
-        number_of_times = 1
-    if not(username or surname):
-         username = 'World'
-    username = username + ' ' + surname
-    return render_template('index3.html', username = username, number_of_times = int(number_of_times))
 
